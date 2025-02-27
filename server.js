4eb7fb1b224082b6ac5503dc9c36bdf4dbc94c02
rayan <rayancooray123456@gmail.com>
2025-02-28 01:18:08 +0530

    connection

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ app.use(express.json());
 app.use(cors());
 app.use(requestIp.mw());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    tls: true,
+    tlsAllowInvalidCertificates: true 
+  });
+  
 
 const TransferSchema = new mongoose.Schema({
   ip: String,
